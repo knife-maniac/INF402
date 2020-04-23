8dd77459f56627032a5954c78d6003285863c93d
Ruben Quast <Ruben.Quast@outlook.fr>
2020-04-23 17:12:41 +0200

    ajout du premier test dans le pipeline

--- unit.test.js
+++ unit.test.js
@@ -2,13 +2,6 @@ const assert = require('assert');
 const futoshiki = require('./futoshiki.js');
 
 
-console.log('Hello world !');
-const result = futoshiki.run('A1\n');
-
-console.log(result.solution_as_array);
-
-
-
 describe('unit tests', function() {
     describe('literal2int()', function() {
         it('must return 1 for A1', function() {
